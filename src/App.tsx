3e14358830764b92bdc8c62f2a03f11644ba365e
Ovsepyan <ovsepyan28@gmail.com>
2023-09-26 21:52:03 +0200

    feat: добавил роутинг и удалил лишние файлы

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.sass";
+import { Route, Routes } from "react-router-dom";
+import { RouteWithLayout } from "./components/RouteWithLayout";
+import { Projects } from "./components/Projects";
+import { Tasks } from "./components/Tasks";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <Routes>
+                <Route path="/" element={<RouteWithLayout />}>
+                    <Route index element={<Projects />} />
+                    <Route path="/:id" element={<Tasks />} />
+                </Route>
+            </Routes>
         </div>
     );
 }
